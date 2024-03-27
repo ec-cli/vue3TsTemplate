fbc7de82eb3efefdb2b227aa4848aac8ba7d2959
任轲 <renke@fpwis.com>
2024-03-27 16:34:56 +0800

    feat: 修改logo图标

--- src/layout/component/SideBar/SidebarLogo.vue
+++ src/layout/component/SideBar/SidebarLogo.vue
@@ -7,10 +7,12 @@
   <div class="sidebar-logo-container" :class="{ collapse: props.collapse }">
     <transition name="sidebar-logo-fade">
       <router-link v-if="props.collapse" key="collapse" to="/">
-        <img src="@/assets/imgs/logo.png" alt="" class="sidebar-logo" />
+        <div class="sidebar-logo">公司</div>
+        <!-- <img src="@/assets/imgs/logo.png" alt="" class="sidebar-logo" /> -->
       </router-link>
       <router-link v-else key="expand" to="/">
-        <img src="@/assets/imgs/logo-text.png" alt="" class="sidebar-logo-text" />
+        <div class="sidebar-logo-text">xxxx公司模板</div>
+        <!-- <img src="@/assets/imgs/logo-text.png" alt="" class="sidebar-logo-text" /> -->
       </router-link>
     </transition>
   </div>
@@ -40,14 +42,18 @@ const props = defineProps({
   .sidebar-logo-text {
     width: 200px;
     vertical-align: middle;
+    color: #fff;
+    font-size: 20px;
   }
 }
 .collapse {
   .sidebar-logo {
     width: 28px;
     height: 28px;
-    vertical-align: middle;
+    // vertical-align: middle;
     display: inline-block;
+    color: #fff;
+    font-size: 12px;
   }
   .sidebar-logo-text {
     display: none;
